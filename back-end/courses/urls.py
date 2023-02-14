aad965ce8932b0821190f045db2da156da873be0
ogakip <pauloogaki.principal@gmail.com>
2023-02-14 11:21:01 -0300

    feature: add course views to project url

--- back-end/courses/urls.py
+++ back-end/courses/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CoursesSimpleViews
+from .views import CoursesSimpleViews, CoursesComplexViews
 
 urlpatterns = [
     path('cursos/', CoursesSimpleViews.as_view())
+    path('cursos/<course_id>', CoursesComplexViews.as_view())
 ]
