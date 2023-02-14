6cfca0432393d9ba1b4f25879b32279541a28c7f
ogakip <pauloogaki.principal@gmail.com>
2023-02-14 11:14:01 -0300

    feature: create write and read of course view

--- back-end/courses/views.py
+++ back-end/courses/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Courses
+from .serializers import CoursesSerializer
 
-# Create your views here.
+class CoursesSimpleViews(APIView):
+    def get(self, request):
+        get_all_courses = Courses.objects.all()
+        serializer = CoursesSerializer(get_all_courses, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        course_data = CoursesSerializer(data=request.data)
+        is_valid_data = course_data.is_valid(raise_exception=True)
+
+        created_student = Courses.objects.create(**course_data.validated_data)
+        return Response({ 'type': 'success', 'message': 'Successfully registered course' })
