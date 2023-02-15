98a12fe49320caa79b1642df04f385c56a4b0d34
ogakip <pauloogaki.principal@gmail.com>
2023-02-15 19:54:36 -0300

    feature: create students page and add to routes

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
+import { Students } from '../pages/students-page';
 
 export const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <div>main page</div>,
+		element: <Students/>,
 	},
 ]);
