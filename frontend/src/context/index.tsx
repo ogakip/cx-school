b663178bfafc0f94ce5ffe19abea85121085e890
ogakip <pauloogaki.principal@gmail.com>
2023-02-15 18:30:42 -0300

    feature: add course reqs

--- frontend/src/context/index.tsx
+++ frontend/src/context/index.tsx
@@ -1,9 +1,14 @@
 import React, { FC, PropsWithChildren } from 'react';
 import { ProviderProps } from '../interfaces/context';
 import { StudentsProvider } from './students';
+import { CourseProvider } from './courses';
 
 export const ContextProvider: FC<PropsWithChildren<ProviderProps>> = ({
 	children,
 }): JSX.Element => {
-	return <StudentsProvider>{children}</StudentsProvider>;
+	return (
+		<CourseProvider>
+			<StudentsProvider>{children}</StudentsProvider>
+		</CourseProvider>
+	);
 };
--- frontend/src/interfaces/context/courses/index.ts
+++ frontend/src/interfaces/context/courses/index.ts
@@ -1,5 +1,23 @@
+import { StudentProps } from '../students';
+
 export interface CourseProps {
     title: string,
     description: string,
     duration: number,
+}
+
+export interface OptionalCourseProps {
+    title?: string,
+    description?: string,
+    duration?: number,
+}
+
+export interface CourseContextProps {
+    courses: CourseProps[],
+    courseStudents: StudentProps[],
+    getAllCourses: () => void,
+    createCourse: (data: CourseProps) => void,
+    updateCourse: (data: OptionalCourseProps, course_id: number) => void,
+    deleteCourse: (course_id: number) => void,
+    listCourseStudents: (course_id: number) => void,
 }
